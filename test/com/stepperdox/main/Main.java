326ee5ab5295bfcbdc7e7b35185bdd641e0a68b6
StepperDox <34612171+StepperDox@users.noreply.github.com>
2019-11-04 10:58:57 -0700

    Create test class

---
+++ test/com/stepperdox/main/Main.java
@@ -0,0 +1,15 @@
+package com.stepperdox.main;
+
+import com.stepperdox.fusion.game.Game;
+
+/**
+*  @author StepperDox
+*  Created on Oct 16, 2019
+*/
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+	}
+}
